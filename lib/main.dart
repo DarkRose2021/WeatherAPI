62763198c7c4ca360e45786258b9c8806b3117f4
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-02-26 09:47:10 -0700

    worked on the login page
    Going to move these out to other dart files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_api/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -42,63 +43,6 @@ BottomNavigationBar navBar(int index) {
   );
 }
 
-class Profile extends StatefulWidget {
-  const Profile({super.key});
-
-  @override
-  State<Profile> createState() => _Profile();
-}
-
-class _Profile extends State<Profile> {
-  int _currentIndex;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          BackButton(
-            onPressed: () => {
-              Navigator.pop(context)
-            },
-          )
-        ],
-      ),
-      body: Column(),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Matches',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.cloud),
-            label: 'Profile',
-          ),
-        ],
-        onTap: (value) => {
-          // MaterialPageRoute placeToGo;
-          // switch(value){
-          // }
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const MyHomePage(
-                title: 'Homepage',
-              ),
-            ),
-          ),
-          setState(() => _currentIndex = value),
-        },
-      ),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -123,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   bool? isChecked = true;
-  int _currentIndex;
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
