c3d2dbe486ea93e86c2d2f2e820ac546f7126adf
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-02-26 09:02:01 -0700

    in class demo

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,93 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    dynamic appTitle = "Wether Dating App";
+    dynamic appTitle = "Weather Dating App";
     return MaterialApp(
       title: appTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
       ),
+      // home: const Profile(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+BottomNavigationBar navBar(int index) {
+  return BottomNavigationBar(
+    currentIndex: index,
+    items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.favorite),
+        label: 'Matches',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.cloud),
+        label: 'Profile',
+      ),
+    ],
+    onTap: (value) => {},
+  );
+}
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _Profile();
+}
+
+class _Profile extends State<Profile> {
+  int _currentIndex;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          BackButton(
+            onPressed: () => {
+              Navigator.pop(context)
+            },
+          )
+        ],
+      ),
+      body: Column(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Matches',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cloud),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (value) => {
+          // MaterialPageRoute placeToGo;
+          // switch(value){
+          // }
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const MyHomePage(
+                title: 'Homepage',
+              ),
+            ),
+          ),
+          setState(() => _currentIndex = value),
+        },
+      ),
     );
   }
 }
@@ -29,7 +108,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   Container buildProfileCard(String profileType) {
     return Container(
       width: 100,
@@ -45,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   bool? isChecked = true;
-
+  int _currentIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +150,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
+        currentIndex: _currentIndex,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.favorite),
@@ -87,6 +165,17 @@ class _MyHomePageState extends State<MyHomePage> {
             label: 'Profile',
           ),
         ],
+        onTap: (value) => {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const MyHomePage(
+                title: 'Homepage',
+              ),
+            ),
+          ),
+          setState(() => _currentIndex = value),
+        },
       ),
     );
   }
